0b66b142632348c248128a037be8c7e8100e5ae1
Matthias Heinz <heinzmc@ornl.gov>
2025-04-04 13:53:46 -0400

    Add a triton run with updated B-type pool from Chenyi

--- 0002_triton_short/papenbrock_adapt_deuteron.py
+++ 0002_triton_short/papenbrock_adapt_deuteron.py
@@ -18,8 +18,8 @@ import os
 
 def main():
 
-    # N = 16 # deuteron
-    N = 24 # Triton
+    N = 16 # deuteron
+    # N = 24 # Triton
 
     if N==16:
         from papenbrock_ham_deuteron import get_info
@@ -47,9 +47,9 @@ def main():
     @qml.qnode(dev, interface="jax")
     def vqe(tau,ops,H):
 
-        qml.X(0)
-        qml.X(1)
-        qml.X(2)
+        # qml.X(4)
+        qml.X(5)
+        qml.X(6)
         for _,op in enumerate(ops):
             qml.TrotterProduct(op,time=tau[_],n=1)
         return qml.expval(H)
@@ -60,7 +60,7 @@ def main():
     opt = optax.adam(learning_rate=0.1)
     theta = []
 
-    max_iter = 300
+    max_iter = 100
     operators = []
     energy = []
     indices = []
@@ -75,14 +75,13 @@ def main():
         ## comment all of this if you wish to do full vqe
         gradients = []
         # random.shuffle(operators)
-        print(vqe(theta,operators,Hamiltonian))
         for op in pools:
             theta_ = list(theta) + list(0*jax.random.normal(key,1)/10000)
 
             theta_ = jax.numpy.array(theta_)
             operators_ = operators.copy() + list([op])
 
-            gradient = jax.grad(lambda x: vqe(x,operators_,Hamiltonian)[0][0])(theta_)
+            gradient = jax.grad(lambda x: vqe(x,operators_,Hamiltonian))(theta_)
             # print(gradient)
 
             if len(gradient)>1:
@@ -114,12 +113,12 @@ def main():
 
         pbar = tqdm.tqdm(range(10), desc="Optimizing")
         for ii in range(10):
-            gradient = jax.grad(lambda x: vqe(x,operators,Hamiltonian)[0][0])(theta)
+            gradient = jax.grad(lambda x: vqe(x,operators,Hamiltonian))(theta)
 
             updates, opt_state = opt.update(gradient, opt_state)
             theta = optax.apply_updates(theta, updates)
 
-            energy.append(vqe(theta,operators,Hamiltonian)[0][0])
+            energy.append(vqe(theta,operators,Hamiltonian))
             pbar.set_description(f"Best cost: {np.min(np.array(energy)):.6f}, {energy[-1]}")
             if i>10:
                 if abs(energy[-1]-energy[-5])<10**-4:
@@ -129,9 +128,9 @@ def main():
         print(energy[-1],np.min(np.array(energy)))
         plt.figure()
         plt.plot(energy)
-        plt.savefig('lattice_energy_optimization_triton.png')
-        np.save('results/nuclear/energy_triton.npy',energy)
-        np.save('results/nuclear/indices_triton.npy',indices)
+        plt.savefig('lattice_energy_optimization.png')
+        np.save('results/nuclear/energy.npy',energy)
+        np.save('results/nuclear/indices.npy',indices)
 
 
 if __name__ == '__main__':
