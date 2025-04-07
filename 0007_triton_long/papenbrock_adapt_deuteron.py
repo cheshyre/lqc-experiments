e6b446625b587c7126c6abab5c8e69608a3253a5
Matthias Heinz <heinzmc@ornl.gov>
2025-04-07 16:43:58 -0400

    Big VQE run

---
+++ 0007_triton_long/papenbrock_adapt_deuteron.py
@@ -0,0 +1,137 @@
+import pennylane as qml
+from jax import numpy as np
+import jax
+import scipy
+# import numpy as np
+# jax.config.update("jax_platform_name", "cpu")
+# jax.config.update('jax_enable_x64', True)
+
+import random
+import optax
+import tqdm
+import matplotlib.pyplot as plt
+import os
+
+
+
+
+
+def main():
+
+    N = 16 # deuteron
+    # N = 24 # Triton
+
+    if N==16:
+        from papenbrock_ham_deuteron import get_info
+        # H = qml.utils.sparse_hamiltonian(Hamiltonian)
+        # H = Hamiltonian.sparse_matrix()
+        #
+        # gs = scipy.sparse.linalg.eigs(H,k=1,which='SR',return_eigenvectors=False)
+        gs = -2.483
+    else:
+        from papenbrock_ham_triton import get_info
+        gs = -72
+
+    print('starting')
+    Hamiltonian, pools = get_info()
+
+    # H = qml.utils.sparse_hamiltonian(Hamiltonian)
+    # H = Hamiltonian.sparse_matrix()
+    # gs = scipy.sparse.linalg.eigs(H,k=1,which='SR',return_eigenvectors=False) # = -72.
+    # print(gs)
+    print('size pool ',len(pools))
+
+    dev = qml.device("lightning.gpu", wires=range(N))
+    # dev = qml.device("lightning.qubit", wires=range(N))# on cpu
+
+    @qml.qnode(dev, interface="jax")
+    def vqe(tau,ops,H):
+
+        # qml.X(4)
+        qml.X(5)
+        qml.X(6)
+        for _,op in enumerate(ops):
+            qml.TrotterProduct(op,time=tau[_],n=1)
+        return qml.expval(H)
+
+
+
+    key = jax.random.key(42)
+    opt = optax.adam(learning_rate=0.1)
+    theta = []
+
+    max_iter = 100
+    operators = []
+    energy = []
+    indices = []
+    # print(pools)
+    ### VQE ###
+    # operators = pools
+    # theta = jax.random.normal(key,len(pools))
+
+    print('starting')
+    for i in range(max_iter):
+        ### ADAPT ###
+        ## comment all of this if you wish to do full vqe
+        gradients = []
+        # random.shuffle(operators)
+        for op in pools:
+            theta_ = list(theta) + list(0*jax.random.normal(key,1)/10000)
+
+            theta_ = jax.numpy.array(theta_)
+            operators_ = operators.copy() + list([op])
+
+            gradient = jax.grad(lambda x: vqe(x,operators_,Hamiltonian))(theta_)
+            # print(gradient)
+
+            if len(gradient)>1:
+                gradient = [gradient[-1]]
+
+            gradients.append(gradient)
+
+        gradients = np.absolute(np.array(gradients).reshape(-1))
+        nbr = 3
+
+        sorted_index = np.argsort(gradients)[-nbr:]
+        # print((gradients))
+        # sorted_index = [np.argmax(gradients)]
+        for  index in sorted_index:
+            indices.append(index)
+            op = pools[index]
+            print('adding operator {} with gradient {} '.format(index, gradients[index]))
+            operators.append(op)
+            key = jax.random.key(42+i)
+            theta =  list(theta.copy()) + list(0*jax.random.normal(key,1)/10000)
+
+        theta = jax.numpy.array(theta)
+
+        ### ADAPT ####
+
+
+        opt = optax.adam(learning_rate=0.01)
+        opt_state = opt.init(theta)
+
+        pbar = tqdm.tqdm(range(10), desc="Optimizing")
+        for ii in range(10):
+            gradient = jax.grad(lambda x: vqe(x,operators,Hamiltonian))(theta)
+
+            updates, opt_state = opt.update(gradient, opt_state)
+            theta = optax.apply_updates(theta, updates)
+
+            energy.append(vqe(theta,operators,Hamiltonian))
+            pbar.set_description(f"Best cost: {np.min(np.array(energy)):.6f}, {energy[-1]}")
+            if i>10:
+                if abs(energy[-1]-energy[-5])<10**-4:
+                    break
+
+
+        print(energy[-1],np.min(np.array(energy)))
+        plt.figure()
+        plt.plot(energy)
+        plt.savefig('lattice_energy_optimization.png')
+        np.save('results/nuclear/energy.npy',energy)
+        np.save('results/nuclear/indices.npy',indices)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 0007_triton_long/script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --account=scalannm
+#SBATCH --job-name=LatticeQC_0007_triton_long
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=48
+#SBATCH --partition=booster
+#SBATCH --time=24:00:00
+#SBATCH --mail-user=heinzmc@ornl.gov
+#SBATCH --mail-type=END,FAIL
+#SBATCH --output=OUTPUT.txt
+#SBATCH --error=ERROR.txt
+
+module load Python CUDA
+cd ~/code/0007_triton_long
+python3 papenbrock_adapt_triton.py   
+
