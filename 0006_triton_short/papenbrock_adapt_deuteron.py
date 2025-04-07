d2a14c8e1e2323376c4e70c8759122addf8b56ee
Matthias Heinz <heinzmc@ornl.gov>
2025-04-07 11:19:48 -0400

    New job just running VQE

--- 0006_triton_short/papenbrock_adapt_deuteron.py
+++ 0006_triton_short/papenbrock_adapt_deuteron.py
@@ -18,8 +18,8 @@ import os
 
 def main():
 
-    # N = 16 # deuteron
-    N = 24 # Triton
+    N = 16 # deuteron
+    # N = 24 # Triton
 
     if N==16:
         from papenbrock_ham_deuteron import get_info
@@ -47,9 +47,9 @@ def main():
     @qml.qnode(dev, interface="jax")
     def vqe(tau,ops,H):
 
-        qml.X(0)
-        qml.X(1)
-        qml.X(2)
+        # qml.X(4)
+        qml.X(5)
+        qml.X(6)
         for _,op in enumerate(ops):
             qml.TrotterProduct(op,time=tau[_],n=1)
         return qml.expval(H)
@@ -60,7 +60,7 @@ def main():
     opt = optax.adam(learning_rate=0.1)
     theta = []
 
-    max_iter = 300
+    max_iter = 100
     operators = []
     energy = []
     indices = []
@@ -75,14 +75,13 @@ def main():
         ## comment all of this if you wish to do full vqe
         gradients = []
         # random.shuffle(operators)
-        print(vqe(theta,operators,Hamiltonian))
         for op in pools:
             theta_ = list(theta) + list(0*jax.random.normal(key,1)/10000)
 
             theta_ = jax.numpy.array(theta_)
             operators_ = operators.copy() + list([op])
 
-            gradient = jax.grad(lambda x: vqe(x,operators_,Hamiltonian)[0][0])(theta_)
+            gradient = jax.grad(lambda x: vqe(x,operators_,Hamiltonian))(theta_)
             # print(gradient)
 
             if len(gradient)>1:
@@ -91,16 +90,15 @@ def main():
             gradients.append(gradient)
 
         gradients = np.absolute(np.array(gradients).reshape(-1))
-        nbr = 90
+        nbr = 3
 
         sorted_index = np.argsort(gradients)[-nbr:]
         # print((gradients))
         # sorted_index = [np.argmax(gradients)]
-        print(f"Adding all {nbr} operators -> VQE")
         for  index in sorted_index:
             indices.append(index)
             op = pools[index]
-            # print('adding operator {} with gradient {} '.format(index, gradients[index]))
+            print('adding operator {} with gradient {} '.format(index, gradients[index]))
             operators.append(op)
             key = jax.random.key(42+i)
             theta =  list(theta.copy()) + list(0*jax.random.normal(key,1)/10000)
@@ -113,14 +111,14 @@ def main():
         opt = optax.adam(learning_rate=0.01)
         opt_state = opt.init(theta)
 
-        pbar = tqdm.tqdm(range(100), desc="Optimizing")
-        for ii in range(100):
-            gradient = jax.grad(lambda x: vqe(x,operators,Hamiltonian)[0][0])(theta)
+        pbar = tqdm.tqdm(range(10), desc="Optimizing")
+        for ii in range(10):
+            gradient = jax.grad(lambda x: vqe(x,operators,Hamiltonian))(theta)
 
             updates, opt_state = opt.update(gradient, opt_state)
             theta = optax.apply_updates(theta, updates)
 
-            energy.append(vqe(theta,operators,Hamiltonian)[0][0])
+            energy.append(vqe(theta,operators,Hamiltonian))
             pbar.set_description(f"Best cost: {np.min(np.array(energy)):.6f}, {energy[-1]}")
             if i>10:
                 if abs(energy[-1]-energy[-5])<10**-4:
@@ -130,9 +128,9 @@ def main():
         print(energy[-1],np.min(np.array(energy)))
         plt.figure()
         plt.plot(energy)
-        plt.savefig('lattice_energy_optimization_triton.png')
-        np.save('results/nuclear/energy_triton.npy',energy)
-        np.save('results/nuclear/indices_triton.npy',indices)
+        plt.savefig('lattice_energy_optimization.png')
+        np.save('results/nuclear/energy.npy',energy)
+        np.save('results/nuclear/indices.npy',indices)
 
 
 if __name__ == '__main__':
---
+++ 0006_triton_short/script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --account=scalannm
+#SBATCH --job-name=LatticeQC_0006_triton_short
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=48
+#SBATCH --partition=develbooster
+#SBATCH --time=2:00:00
+#SBATCH --mail-user=heinzmc@ornl.gov
+#SBATCH --mail-type=END,FAIL
+#SBATCH --output=OUTPUT.txt
+#SBATCH --error=ERROR.txt
+
+module load Python CUDA
+cd ~/code/0006_triton_short
+python3 papenbrock_adapt_triton.py   
+
